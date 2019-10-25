72164bc55e34fbe156263c6829318395507506b0
Jury Razumau <jurazumau@gmail.com>
2019-10-25 11:55:24 +0300

    add setup.py, move to 1.0.1

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r") as rs:
+    requirements = rs.read()
+
+setuptools.setup(
+    name="chgk_rating_client",
+    version="1.0.1",
+    author="Jury Razumau",
+    author_email="jurazumau@gmal.com",
+    description="Клиент для API rating.chgk.info",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=requirements,
+    url="https://github.com/razumau/chgk_rating_client",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
